bad504c482bf6feec98bd48ce3732a236450580c
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-10-15 10:19:36 +1300

    WIP

--- client/ReleaseProjectFromBitbucketHook.php
+++ client/ReleaseProjectFromBitbucketHook.php
@@ -3,10 +3,8 @@
 $obj = new ReleaseProjectFromBitbucketHook();
 $obj->run();
 
-
-class ReleaseProjectFromBitbucketHook 
+class ReleaseProjectFromBitbucketHook
 {
-
     private $allow = false;
     private $ip = '';
     private $allowedCIDRS = [];
@@ -14,102 +12,104 @@ class ReleaseProjectFromBitbucketHook
     private $webhookSecretProvided = '';
     private $releaseScript = '';
 
-      
     public function run()
     {
-        if($this->getVars()) {
-            if($this->securityChecks()) {
-                shell_exec('bash '.$this->releaseScript);
+        if ($this->getVars()) {
+            if ($this->securityChecks()) {
+                shell_exec('bash ' . $this->releaseScript);
             }
         }
     }
-  
-    private function getVars() :bool
+
+    private function getVars(): bool
     {
         $this->ip = $_SERVER['REMOTE_ADDR'];
         $this->webhookSecret = getenv('SS_RELEASE_TOKEN');
         $this->releaseScript = getenv('SS_RELEASE_SCRIPT');
         $this->webhookSecretProvided = empty($_GET['ts']) ? '' : $_GET['ts'];
-        if($this->basicCheck() ) {  
+        if ($this->basicCheck()) {
             $this->getAtlassionIpRanges();
+
             return true;
         }
+
         return false;
     }
-    
-    private function basicCheck() : bool
+
+    private function basicCheck(): bool
     {
-        foreach(['ip', 'webhohookSecret', 'releaseScript', 'webhookSecretProvided'] as $var) {
-            if(empty($this->{$var})) {
-                user_error('You need to set '.$var, E_USER_NOTICE);
+        foreach (['ip', 'webhohookSecret', 'releaseScript', 'webhookSecretProvided'] as $var) {
+            if (empty($this->{$var})) {
+                user_error('You need to set ' . $var, E_USER_NOTICE);
                 $this->abort();
+
                 return false;
             }
         }
+
         return true;
     }
-    
-    private function isIpInRange($ip, $range) : bool
+
+    private function isIpInRange($ip, $range): bool
     {
         list($range, $netmask) = explode('/', $range, 2);
         $ipLong = ip2long($ip);
         $rangeLong = ip2long($range);
         $wildcard = pow(2, (32 - $netmask)) - 1;
-        $netmask = ~ $wildcard;
-        
-        return (($ipLong & $netmask) == ($rangeLong & $netmask));
+        $netmask = ~$wildcard;
+
+        return ($ipLong & $netmask) === ($rangeLong & $netmask);
     }
-    
-    private function abort() 
+
+    private function abort()
     {
         header('HTTP/1.1 403 Forbidden');
         exit;
     }
-    
+
     /**
-    * Get Bitbucket IP range
-    */
-    private function getAtlassionIpRanges() 
+     * Get Bitbucket IP range.
+     */
+    private function getAtlassionIpRanges()
     {
         $atlassian = file_get_contents('https://ip-ranges.atlassian.com/');
         $ranges = json_decode($atlassian);
-        /**
-        * Get CIDRs (Classless Inter-Domain Routing)
-        */
-        foreach($ranges->items as $item) {
+        // Get CIDRs (Classless Inter-Domain Routing)
+        foreach ($ranges->items as $item) {
             $this->allowedCIDRS[] = $item->cidr;
         }
     }
 
-    private function securityChecks() 
+    private function securityChecks()
     {
-        
-        /**
-        * (1) Primary check
-        * 
-        * Block if for some reason we couldn't get a response from Atlassian.
-        */
-        if (!empty($this->allowedCIDRS)) {
+        /*
+         * (1) Primary check
+         *
+         * Block if for some reason we couldn't get a response from Atlassian.
+         */
+        if (! empty($this->allowedCIDRS)) {
             $this->abort();
+
+            return false;
+        }
+
+        /*
+         * (2) Secondary check (Additional/Optional)
+         *
+         * Token validation, that comes from the Bitbucket Webhook setup
+         */
+        if ($this->webhookSecretProvided !== $this->webhookSecret) {
+            $this->abort();
+
             return false;
         }
 
         /**
-        * (2) Secondary check (Additional/Optional)
-        * 
-        * Token validation, that comes from the Bitbucket Webhook setup
-        */
-        if($this->webhookSecretProvided !== $this->webhookSecret) {
-            $this->abort();
-            return false;
-        }
-
-        /**
-        * (3) Tertiary check
-        * 
-        * IP validation - only allow requests for Atlassian IPs
-        */
-       $allow = false;
+         * (3) Tertiary check.
+         *
+         * IP validation - only allow requests for Atlassian IPs
+         */
+        $allow = false;
         foreach ($allowedCIDRS as $cidr) {
             $allow = isIpInRange($ip, $cidr);
             if ($allow) {
@@ -117,13 +117,12 @@ class ReleaseProjectFromBitbucketHook
             }
         }
 
-        if (!$allow) {
+        if (! $allow) {
             $this->abort();
+
             return false;
-        }  
-        return true;      
+        }
+
+        return true;
     }
-
-
-    
 }
