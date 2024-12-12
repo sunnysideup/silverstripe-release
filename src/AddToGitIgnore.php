75467826557abfb22f95afe498cf998d10f30fce
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-12-12 22:25:00 +1300

    FIX: bugfixes

--- src/AddToGitIgnore.php
+++ src/AddToGitIgnore.php
@@ -17,18 +17,21 @@ class AddToGitIgnore implements Flushable
     protected static function addToGitIgnore()
     {
         $filePath = Director::baseFolder().'/'.'.gitignore';
-        $linesToAdd = ['release.log', 'release_running'];
-
+        $linesToAdd = ['/release.log', '/release_running'];
         if (!file_exists($filePath)) {
             file_put_contents($filePath, implode(PHP_EOL, $linesToAdd) . PHP_EOL);
             return;
         }
-
         $currentContent = file($filePath, FILE_IGNORE_NEW_LINES);
         $missingLines = array_filter($linesToAdd, fn ($line) => !in_array($line, $currentContent));
 
         if (!empty($missingLines)) {
-            file_put_contents($filePath, PHP_EOL . implode(PHP_EOL, $missingLines) . PHP_EOL, FILE_APPEND);
+            if (is_writable($filePath)) {
+                file_put_contents($filePath, PHP_EOL . implode(PHP_EOL, $missingLines) . PHP_EOL, FILE_APPEND);
+            } else {
+                die('ERROR: Please add the following lines to your .gitignore file: ' . PHP_EOL . implode(PHP_EOL, $missingLines) . PHP_EOL);
+            }
         }
+
     }
 }
