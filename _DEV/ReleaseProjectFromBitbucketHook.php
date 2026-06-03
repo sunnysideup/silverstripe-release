71de90ce302500ce0c92f27699c017b607cbdc83
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-06-03 14:30:17 +1200

    FIX: cleanup

--- _DEV/ReleaseProjectFromBitbucketHook.php
+++ _DEV/ReleaseProjectFromBitbucketHook.php
@@ -1,240 +1,240 @@
 <?php
 
-$obj = new ReleaseProjectFromBitbucketHook();
-$obj->run();
+// $obj = new ReleaseProjectFromBitbucketHook();
+// $obj->run();
 
-class Add
-{
-    /**
-     * The directory where the .env file can be located.
-     *
-     * @var string
-     */
-    protected $path = '.env';
+// class Add
+// {
+//     /**
+//      * The directory where the .env file can be located.
+//      *
+//      * @var string
+//      */
+//     protected $path = '.env';
 
-    private $allow = false;
+//     private $allow = false;
 
-    private $ip = '';
+//     private $ip = '';
 
-    private $allowedCIDRS = [
-        '127.0.0.1/0',
-    ];
+//     private $allowedCIDRS = [
+//         '127.0.0.1/0',
+//     ];
 
-    private $webhookSecret = '';
+//     private $webhookSecret = '';
 
-    private $webhookSecretProvided = '';
+//     private $webhookSecretProvided = '';
 
-    private $releaseScript = '';
+//     private $releaseScript = '';
 
-    private $absoluteDirOfThisScript = '';
+//     private $absoluteDirOfThisScript = '';
 
-    private $envType = '';
+//     private $envType = '';
 
-    public function run()
-    {
-        if ($this->getVars()) {
-            if ($this->securityChecks()) {
-                $this->runInner();
-            } else {
-                $this->abort('wrong credentials');
-            }
-        } else {
-            $this->abort('no getVars');
-        }
-    }
+//     public function run()
+//     {
+//         if ($this->getVars()) {
+//             if ($this->securityChecks()) {
+//                 $this->runInner();
+//             } else {
+//                 $this->abort('wrong credentials');
+//             }
+//         } else {
+//             $this->abort('no getVars');
+//         }
+//     }
 
-    protected function getVars(): bool
-    {
-        $this->absoluteDirOfThisScript = realpath(dirname(__FILE__));
-        $this->findDotEnvFile();
-        $this->loadDotEnvFile();
-        $this->envType = getenv('SS_ENVIRONMENT_TYPE');
-        $this->ip = filter_var($_SERVER['REQUEST_URI'], FILTER_VALIDATE_IP);
-        $this->webhookSecret = getenv('SS_RELEASE_TOKEN');
-        $this->releaseScript = getenv('SS_RELEASE_SCRIPT') ?: 'vendor/bin/sake-release live';
-        $this->webhookSecretProvided = empty($_GET['ts']) ? '' : $_GET['ts'];
+//     protected function getVars(): bool
+//     {
+//         $this->absoluteDirOfThisScript = realpath(dirname(__FILE__));
+//         $this->findDotEnvFile();
+//         $this->loadDotEnvFile();
+//         $this->envType = getenv('SS_ENVIRONMENT_TYPE');
+//         $this->ip = filter_var($_SERVER['REQUEST_URI'], FILTER_VALIDATE_IP);
+//         $this->webhookSecret = getenv('SS_RELEASE_TOKEN');
+//         $this->releaseScript = getenv('SS_RELEASE_SCRIPT') ?: 'vendor/bin/sake-release live';
+//         $this->webhookSecretProvided = empty($_GET['ts']) ? '' : $_GET['ts'];
 
-        return $this->basicCheck();
-    }
+//         return $this->basicCheck();
+//     }
 
-    protected function securityChecks()
-    {
-        $this->getAtlassionIpRanges();
+//     protected function securityChecks()
+//     {
+//         $this->getAtlassionIpRanges();
 
-        /*
-         * (1) Primary check
-         *
-         * Block if for some reason we couldn't get a response from Atlassian.
-         */
-        if (empty($this->allowedCIDRS)) {
-            $this->abort('No ranges found for bitbucket');
+//         /*
+//          * (1) Primary check
+//          *
+//          * Block if for some reason we couldn't get a response from Atlassian.
+//          */
+//         if (empty($this->allowedCIDRS)) {
+//             $this->abort('No ranges found for bitbucket');
 
-            return false;
-        }
+//             return false;
+//         }
 
-        /*
-         * (2) Secondary check (Additional/Optional)
-         *
-         * Token validation, that comes from the Bitbucket Webhook setup
-         */
-        if ($this->webhookSecretProvided !== $this->webhookSecret) {
-            $this->abort('Token does not match: ');
+//         /*
+//          * (2) Secondary check (Additional/Optional)
+//          *
+//          * Token validation, that comes from the Bitbucket Webhook setup
+//          */
+//         if ($this->webhookSecretProvided !== $this->webhookSecret) {
+//             $this->abort('Token does not match: ');
 
-            return false;
-        }
+//             return false;
+//         }
 
-        /**
-         * (3) Tertiary check.
-         *
-         * IP validation - only allow requests for Atlassian IPs
-         */
-        $allow = false;
-        foreach ($this->allowedCIDRS as $cidr) {
-            $allow = $this->isIpInRange($cidr);
-            if ($allow) {
-                break;
-            }
-        }
+//         /**
+//          * (3) Tertiary check.
+//          *
+//          * IP validation - only allow requests for Atlassian IPs
+//          */
+//         $allow = false;
+//         foreach ($this->allowedCIDRS as $cidr) {
+//             $allow = $this->isIpInRange($cidr);
+//             if ($allow) {
+//                 break;
+//             }
+//         }
 
-        if (! $allow) {
-            $this->abort('Ip (' . $this->ip . ') not in range');
+//         if (! $allow) {
+//             $this->abort('Ip (' . $this->ip . ') not in range');
 
-            return false;
-        }
+//             return false;
+//         }
 
-        return true;
-    }
+//         return true;
+//     }
 
-    protected function runInner()
-    {
-        $optionA = $this->releaseScript;
-        $optionB = $this->absoluteDirOfThisScript . '/' . $this->releaseScript;
-        $optionC = '../../../../' . $this->releaseScript;
-        foreach ([$optionA, $optionB, $optionC] as $scriptPath) {
-            if (file_exists($scriptPath)) {
-                $scriptPath = realpath($scriptPath);
-                if (is_executable($scriptPath)) {
-                    echo '<h1>' . $scriptPath . '</h1>';
-                    $output = shell_exec('bash ' . $scriptPath . ' 2>&1');
-                    echo "<pre>{$output}</pre>";
-                    die('<h1>DONE</h1>');
-                }
+//     protected function runInner()
+//     {
+//         $optionA = $this->releaseScript;
+//         $optionB = $this->absoluteDirOfThisScript . '/' . $this->releaseScript;
+//         $optionC = '../../../../' . $this->releaseScript;
+//         foreach ([$optionA, $optionB, $optionC] as $scriptPath) {
+//             if (file_exists($scriptPath)) {
+//                 $scriptPath = realpath($scriptPath);
+//                 if (is_executable($scriptPath)) {
+//                     echo '<h1>' . $scriptPath . '</h1>';
+//                     $output = shell_exec('bash ' . $scriptPath . ' 2>&1');
+//                     echo "<pre>{$output}</pre>";
+//                     die('<h1>DONE</h1>');
+//                 }
 
-                die($scriptPath . ' is not executable');
-            }
-        }
+//                 die($scriptPath . ' is not executable');
+//             }
+//         }
 
-        $this->abort('Could not find: ' . implode(',', [$optionA, $optionB, $optionC]));
-    }
+//         $this->abort('Could not find: ' . implode(',', [$optionA, $optionB, $optionC]));
+//     }
 
-    private function basicCheck(): bool
-    {
-        foreach (
-            [
-                'absoluteDirOfThisScript',
-                'envType',
-                'ip',
-                'webhookSecret',
-                'webhookSecretProvided',
-                'releaseScript',
-            ] as $var
-        ) {
-            if (empty($this->{$var})) {
-                $this->abort('You need to set ' . $var);
+//     private function basicCheck(): bool
+//     {
+//         foreach (
+//             [
+//                 'absoluteDirOfThisScript',
+//                 'envType',
+//                 'ip',
+//                 'webhookSecret',
+//                 'webhookSecretProvided',
+//                 'releaseScript',
+//             ] as $var
+//         ) {
+//             if (empty($this->{$var})) {
+//                 $this->abort('You need to set ' . $var);
 
-                return false;
-            }
-        }
+//                 return false;
+//             }
+//         }
 
-        return true;
-    }
+//         return true;
+//     }
 
-    private function isIpInRange($range): bool
-    {
-        list($range, $netmask) = explode('/', $range, 2);
-        $netmask = (int) $netmask;
-        $ipLong = ip2long($this->ip);
-        $rangeLong = ip2long($range);
-        $wildcard = pow(2, (32 - $netmask)) - 1;
-        $netmask = ~$wildcard;
+//     private function isIpInRange($range): bool
+//     {
+//         list($range, $netmask) = explode('/', $range, 2);
+//         $netmask = (int) $netmask;
+//         $ipLong = ip2long($this->ip);
+//         $rangeLong = ip2long($range);
+//         $wildcard = pow(2, (32 - $netmask)) - 1;
+//         $netmask = ~$wildcard;
 
-        return ($ipLong & $netmask) === ($rangeLong & $netmask);
-    }
+//         return ($ipLong & $netmask) === ($rangeLong & $netmask);
+//     }
 
-    private function abort(string $reason)
-    {
-        if ($this->isSafeEnvironment()) {
-            echo 'Aborted because of ' . htmlentities($reason);
-        } else {
-            header('HTTP/1.1 403 Forbidden');
-        }
+//     private function abort(string $reason)
+//     {
+//         if ($this->isSafeEnvironment()) {
+//             echo 'Aborted because of ' . htmlentities($reason);
+//         } else {
+//             header('HTTP/1.1 403 Forbidden');
+//         }
 
-        exit;
-    }
+//         exit;
+//     }
 
-    private function isSafeEnvironment(): bool
-    {
-        $test = strtolower($this->envType);
+//     private function isSafeEnvironment(): bool
+//     {
+//         $test = strtolower($this->envType);
 
-        return 'test' === $test || 'dev' === $test;
-    }
+//         return 'test' === $test || 'dev' === $test;
+//     }
 
-    /**
-     * Get Bitbucket IP range.
-     */
-    private function getAtlassionIpRanges()
-    {
-        $atlassian = file_get_contents('https://ip-ranges.atlassian.com/');
-        $ranges = json_decode($atlassian);
-        // Get CIDRs (Classless Inter-Domain Routing)
-        foreach ($ranges->items as $item) {
-            $this->allowedCIDRS[] = $item->cidr;
-        }
-    }
+//     /**
+//      * Get Bitbucket IP range.
+//      */
+//     private function getAtlassionIpRanges()
+//     {
+//         $atlassian = file_get_contents('https://ip-ranges.atlassian.com/');
+//         $ranges = json_decode($atlassian);
+//         // Get CIDRs (Classless Inter-Domain Routing)
+//         foreach ($ranges->items as $item) {
+//             $this->allowedCIDRS[] = $item->cidr;
+//         }
+//     }
 
-    private function findDotEnvFile(): void
-    {
-        $x = 0;
-        $myPath = '.env';
-        $myPathAbsolute = '';
-        while ($x < 30) {
-            $myPathAbsolute = realpath($this->absoluteDirOfThisScript . '/' . $myPath);
-            if (file_exists($myPathAbsolute)) {
-                $x = 999;
-            } else {
-                $myPath = '../' . $myPath;
-                ++$x;
-            }
-        }
+//     private function findDotEnvFile(): void
+//     {
+//         $x = 0;
+//         $myPath = '.env';
+//         $myPathAbsolute = '';
+//         while ($x < 30) {
+//             $myPathAbsolute = realpath($this->absoluteDirOfThisScript . '/' . $myPath);
+//             if (file_exists($myPathAbsolute)) {
+//                 $x = 999;
+//             } else {
+//                 $myPath = '../' . $myPath;
+//                 ++$x;
+//             }
+//         }
 
-        if (! file_exists($myPathAbsolute)) {
-            throw new \InvalidArgumentException(sprintf('%s does not exist', $myPathAbsolute));
-        }
+//         if (! file_exists($myPathAbsolute)) {
+//             throw new \InvalidArgumentException(sprintf('%s does not exist', $myPathAbsolute));
+//         }
 
-        $this->path = $myPathAbsolute;
-    }
+//         $this->path = $myPathAbsolute;
+//     }
 
-    private function loadDotEnvFile(): void
-    {
-        if (! is_readable($this->path)) {
-            throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
-        }
+//     private function loadDotEnvFile(): void
+//     {
+//         if (! is_readable($this->path)) {
+//             throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
+//         }
 
-        $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        foreach ($lines as $line) {
-            if (0 === strpos(trim($line), '#')) {
-                continue;
-            }
+//         $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+//         foreach ($lines as $line) {
+//             if (0 === strpos(trim($line), '#')) {
+//                 continue;
+//             }
 
-            list($name, $value) = explode('=', $line, 2);
-            $name = trim($name);
-            $value = trim($value);
-            $value = trim($value, '"');
-            $value = trim($value, "'");
+//             list($name, $value) = explode('=', $line, 2);
+//             $name = trim($name);
+//             $value = trim($value);
+//             $value = trim($value, '"');
+//             $value = trim($value, "'");
 
-            if (! getenv($name)) {
-                putenv(sprintf('%s=%s', $name, $value));
-            }
-        }
-    }
-}
+//             if (! getenv($name)) {
+//                 putenv(sprintf('%s=%s', $name, $value));
+//             }
+//         }
+//     }
+// }
